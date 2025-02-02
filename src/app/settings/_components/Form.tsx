eeef98e7e53c7b62a600a491c37f8e4e89381a5e
Onos <onosejoor14@gmail.com>
2025-02-02 01:53:16 +0100

    fix typescript error

--- src/app/settings/_components/Form.tsx
+++ src/app/settings/_components/Form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEvent, DragEvent, useState } from "react";
+import { ChangeEvent, DragEvent, FormEvent, useState } from "react";
 import axios from "axios";
 import { mutate } from "swr";
 
